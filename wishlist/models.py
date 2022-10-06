efd6caf4141410590b6fae52c7c79bd68fbf3dee
Syadza Desvianto <syadzadesvianto@gmail.com>
2022-10-06 16:30:31 +0700

    Tutorial 5 (gatau bingung kak sorry)

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,7 +1,13 @@
+from dataclasses import field
 from django.db import models
 
+# Create your models here.
 class BarangWishlist(models.Model):
     nama_barang = models.CharField(max_length=50)
     harga_barang = models.IntegerField()
     deskripsi = models.TextField()
-# Create your models here.
+
+class AddWishlist(forms.ModelForm):
+    class Meta:
+        model = BarangWishlist
+        field = ['nama_barang', 'harga_barang', 'deskripsi']
--- wishlist/views.py
+++ wishlist/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from wishlist.models import BarangWishlist
+from wishlist.models import *
 
 # Create your views here.
 @login_required(login_url='/wishlist/login/')
@@ -21,6 +21,27 @@ def show_wishlist(request):
         'last_login': request.COOKIES['last_login'],
     }
     return render(request, "wishlist.html", context)
+
+def show_wishlist_ajax(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+        'list_barang': data_barang_wishlist,
+        'nama': 'Syadza Nayla Arrana Desvianto',
+        'last_login': request.COOKIES['last_login'],
+    }
+    return render(request, "wishlist_ajax.html", context)
+
+def add_wishlist(request):
+    form = AddWishlist()
+
+    if request.method == "POST":
+        form = AddWishlist(request.POST or None, request.FILES or None)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Added!")
+    
+    context = {'form':form}
+    return render(request, 'add_wishlist.html', context)
 
 def show_xml(request):
     data = BarangWishlist.objects.all()
